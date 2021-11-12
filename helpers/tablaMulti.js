9813638f5dfab99e5fd4e0d921ba5c657ee83695
joalvarado2 <jonathanalva92@gmail.com>
2021-11-12 11:15:30 -0500

    corrigiendo error de generador de texto en el .txt

--- helpers/tablaMulti.js
+++ helpers/tablaMulti.js
@@ -6,9 +6,12 @@ const multi = async (number, listar = false, max) => {
   try {
     const num = number;
     let salida = "";
+    let consola = "";
+
     for (let i = 1; i <= max; i++) {
       let res = num * i;
       salida += `${num} x ${i} = ${res}\n`;
+      consola += `${num} ${"x".yellow} ${i} ${"=".yellow} ${res}\n`;   // esta linea representa la funcion de colors en la consola
     }
 
     fs.appendFile("tabla.txt", salida, (err) => {
@@ -20,7 +23,7 @@ const multi = async (number, listar = false, max) => {
       console.log(`tabla del ${number}`.cyan);
       console.log("===================".rainbow);
 
-      console.log(salida);
+      console.log(consola);
     }
     return salida;
   } catch (error) {
